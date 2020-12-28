dc4d8aa87049338a88c2f535d020e1bd30b4f27b
Kanik Kwatra <Kanik_Kwatra@ad.infosys.com>
2020-12-29 01:01:29 +1100

    adding test case for payee list

---
+++ coding-challenge/src/__tests__/utils.test.ts
@@ -0,0 +1,9 @@
+import { sortPayeeListLodash } from '../api/util'
+
+
+describe("Payee list", () => {
+    it("should render the button component", () => {
+        const list = [{"name":"Zac","payeeType":"LINKED","primary":{"isPrimary":true}},{"name":"Alex","payeeType":"PAY_ANYONE","primary":{"isPrimary":false}},{"name":"Ben","payeeType":"PAY_ANYONE","primary":{"isPrimary":false}},{"name":"Mary","payeeType":"PAY_ANYONE","primary":{"isPrimary":false}},{"name":"Angela","payeeType":"LINKED","primary":{"isPrimary":false}},{"name":"Sam","payeeType":"LINKED","primary":{"isPrimary":false}},{"name":"Zac","payeeType":"LINKED","primary":{"isPrimary":true}},{"name":"Andy","payeeType":"BPAY","primary":{"isPrimary":false}},{"name":"John","payeeType":"BPAY","primary":{"isPrimary":false}},{"name":"Zachary","payeeType":"BPAY","primary":{"isPrimary":false}}]
+        expect(sortPayeeListLodash())).toBe(list)
+    })
+})
